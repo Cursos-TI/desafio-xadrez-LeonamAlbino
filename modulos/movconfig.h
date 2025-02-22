dc19589c1a1faaa45a659d069d51d398e0bc33b7
LeonamAlbino <167349188+LeonamAlbino@users.noreply.github.com>
2025-02-22 12:20:01 -0300

    setting up more things

--- modulos/movconfig.h
+++ modulos/movconfig.h
@@ -1,46 +1,93 @@
 #include<stdio.h>
 void sdtinclear(){
-    int c; 
     //executa o getchar até que todas os caracteres sejam gravados
+    int c; 
     while((c = getchar())!= '\n' && EOF);
 }
 void movlog(int type,char pecanome[]){
-    char direction; int nhouses;
+
+    char direction; int nhouses,diagonal;
     printf("\n<<mover peça>>\n");
 
     switch(type){
         case 1:
         printf("\ndigite a direção desejada!!\n movimentos possiveis\nL para esquerda\nD para baixo\nU para cima\nR para esquerda\n\n");
         break;
+        case 2:
+        printf("digite a direção desejadaz\n\n1. superior esquerdo!   2. superior direito\n                      x\n3. inferior esquerdo    4. canto inferior direito\n");
+        break;
     }
-    direction = getchar();
-    sdtinclear();
-
-    printf("digite o numero de casas a serem percorridas!\n");
-    scanf("%d",&nhouses);
-    sdtinclear();
+    switch(type){
+        case 1:
+        commondirectionset(&direction,&nhouses);
+        break;
+        case 2:
+        diagonaldirectionset(&nhouses,&diagonal);
+        break;
+    }
     
     printf("\nmovendo a peça %s para a ",pecanome);
     fflush(stdout);
-
+    
     switch(type){
-        //common
-        case 1:
-        switch (direction){
-            case 'L': case 'l':
-                printf("Esquerda %d Vezes!\n",nhouses);
-                break;
+//main movements            
+    //common movements 
+        //<cross>
+            case 1:
+            if (pecanome[5]){
+                nhouses=1;
+            }
+
+            if (pecanome[3]){
+                if(nhouses>1){
+                    nhouses=2;
+                }else{
+                    nhouses=1;
+                }
+            }
+
+            switch (direction){
+                case 'L': case 'l':
+                    printf("Esquerda %d Vezes!\n",nhouses);
+                    break;
+                    
+                case 'D': case 'd':
+                    printf("Baixo %d Vezes!\n",nhouses);
+                    break;
+                case 'U': case 'u':
+                    printf("Cima %d Vezes!\n",nhouses);
+                    break;
+                case 'R': case 'r':
+                    printf("Direita %d Vezes!\n",nhouses);
+                    break; 
+            }
+        //</cross>
             
-            case 'D': case 'd':
-                printf("Baixo %d Vezes!\n",nhouses);
-                break;
-            case 'U': case 'u':
-                printf("Cima %d Vezes!\n",nhouses);
-                break;
-            case 'R': case 'r':
-                printf("Direita %d Vezes!\n",nhouses);
-                break; }
-        //diagonal
-        //in L 2 + dir-mv-c-(L|r|u|d)
+        //<diagonal>
+            case 2: 
+
+            break;
+        //</diagonal>
+    //common movement end
+
+    //knight mov
+    //knight mov  end
+    
+//main movements end
     }
+}
+void commondirectionset(char *direction,int *nhouses){
+    *direction = getchar();
+    sdtinclear();
+
+    printf("digite o numero de casas a serem percorridas!\n");
+    scanf("%d",&(*nhouses));
+    sdtinclear();
+}
+void diagonaldirectionset(int *nhouses,int *diagonal){
+    scanf("%d",&diagonal);
+    sdtinclear();
+    printf("\ndigite o numero de casas a serem percorridas!\n");
+    scanf("%d",&(*nhouses));
+    sdtinclear();
 }
--- xadrez.c
+++ xadrez.c
@@ -1,24 +1,69 @@
-#include <stdio.h>
-#include <locale.h>
-#include "modulos/movconfig.h"
-int tower();
+//<includes>
+    #include <stdio.h>
+        #include <locale.h>
+        #include "modulos/movconfig.h"
+//</includes>
+
+//<functionset>
+    int tower(char *pecanome);
+    int paw(char *pecanome);
+    int bishop(char *pecanome);
+//</functionset>
+
 int main(void) {
-    
+//var area
+    //um array que representa as peças a serem trabalhadas no projeto
+    char *pecanome={
+        "torre",
+        "bispo",
+        "cavalo",
+        "peão",
+        "rainha",
+        "rei"
+    };
+    //seleção de movimento exclusivo do rei e da rainha que podem escolher entre os movimentos common e diagonal excluindo o cavalo tem tem um movimento proprio exclusivo
+    int tslcqk[2]={1,2}; 
+    //armazena o <user selection> do <menu>
     int pieceselector;
+//var area fim
+
+//menu area
+    //<userselectioandexecuter>
+        printf("qual peça deseja mover?\n\n1. torre\n");
+        scanf("%d",&pieceselector);
+        //faz uma faxina no buffer de entrada pra n dar problema depois
+        int c;
+        while((c = getchar())!= '\n' && EOF);;
+        switch(pieceselector){
+            case 1:
+            tower(&pecanome);
+            break;
+            case 2:
+            bishop(&pecanome);
+            break;
+        }
+    //</userselectioandexecuter>
+//menu area fim
     
-    printf("qual peça deseja mover?\n\n1. torre\n");
-    scanf("%d",&pieceselector);
-    int c; 
-    while((c = getchar())!= '\n' && EOF);;
- 
+//<EOP>
+    //<pre-end>
     
-    switch(pieceselector){
-        case 1:
-        tower();
+    //</pre-end>
+
+    //<end>
+        return 0;
+    //</end>
+        
+//</EOP>
     }
-    return 0;
-}
-int tower(){
-    char pecanome[20]="torre";
-        movlog(1,pecanome);
-}
+//functions area
+    int paw(char *pecanome){
+        movlog(1,pecanome[3]);
+    }
+    int tower(char *pecanome){
+        movlog(1,pecanome[0]);
+    }
+    int bishop(char *pecanome){
+        movlog(2,pecanome[1]);
+    }
+//functions area end
